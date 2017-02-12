753f9be1528a5f0d57a1bc368811f5aaaffeac3f
jackie <jackieyan.97@hotmail.com>
2017-02-11 19:16:37 -0500

    added course class, added to student and instructor classes

---
+++ src/Course.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jackie on 2017-02-11.
+ */
+public class Course
+{
+    String cname;
+    String description;
+
+    public Course(String cname, String description)
+    {
+        this.cname = cname;
+        this.description = description;
+    }
+}
